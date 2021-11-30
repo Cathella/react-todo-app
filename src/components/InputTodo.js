f215e2b4c8e33ca1ec2a0c9bdfe9567188ba8d26
Cathella <ncathie214@gmail.com>
2021-11-30 15:33:41 +0300

    Add styling to the UI

--- src/components/InputTodo.js
+++ src/components/InputTodo.js
@@ -25,15 +25,16 @@ class InputTodo extends Component {
 
   render() {
     return (
-      <form onSubmit={this.handleSubmit}>
+      <form onSubmit={this.handleSubmit} className="form-container">
         <input
-          name="title"
-          type="text" 
-          onChange={this.onChange} 
-          placeholder="Add todo..." 
+          type="text"
+          className="input-text"
+          placeholder="Add todo..."
           value={this.state.title}
+          name="title"
+          onChange={this.onChange}
         />
-        <button>Submit</button>
+        <button className="input-submit">Submit</button>
       </form>
     )
   }
