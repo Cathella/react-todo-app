527a26745fe05018e382221bd5e894b4cce1aa1a
Cathella <ncathie214@gmail.com>
2021-11-30 03:38:13 +0300

    Add strict mode

---
+++ src/components/TodoContainer.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+class TodoContainer extends React.Component {
+  render() {
+    return (
+      <div>
+        <h1>Hello from the create React App</h1>
+        <p>I am in a React Component</p>
+      </div>
+    );
+  }
+}
+
+export default TodoContainer;
